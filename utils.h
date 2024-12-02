da379a6d933104d3f430aa0afd9dec22e6d2727f
andriana <97804237+andriana2@users.noreply.github.com>
2024-12-02 17:45:03 +0100

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,27 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// #include <iostream>
+// #include <stdexcept>
+#include <QString>
+// #include <string>
+
+// enum Move {
+//     Recto,
+//     Atras,
+//     Giro_Izquierda,
+//     Giro_Derecha,
+//     Mas_Rapido,
+//     Mas_Lento
+// };
+
+// // Función que convierte el enum Move a un string
+// std::string MoveToString(Move move);
+
+// // Función que convierte un string a un valor enum Move
+// Move stringToMove(const std::string& str);
+
+// std::string obtenerIP();
+QString obtenerIP();
+
+#endif // UTILS_H
