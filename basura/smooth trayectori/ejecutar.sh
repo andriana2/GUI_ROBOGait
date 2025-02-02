d027cbe3adc9bfe2c959370aa6ddf32900c8f24f
andriana2 <andriana.dov@gmail.com>
2025-02-02 13:19:25 +0100

    incluimos el filtro de datos junto al suavidado de curvas

--- basura/smooth trayectori/ejecutar.sh
+++ basura/smooth trayectori/ejecutar.sh
@@ -2,11 +2,15 @@
 
 # Lista de archivos a compilar y ejecutar
 archivos=(
+    # "reducir_punto.cpp"
     # "bezier.cpp"
+    "bezier2.cpp"
     # "suavizarTrayectoriaChaikin.cpp"
+    # "chaikin.cpp"
     # "suavizarTrayectoriaKalman.cpp"
-    "suavizarTrayectoriaMediaMovil.cpp"
-    "trayectoria_corner_cutting.cpp"
+    # "suavizarTrayectoriaMediaMovil.cpp"
+    # "trayectoria_corner_cutting.cpp"
+    "corner_cutting.cpp"
 )
 
 # Compilar y ejecutar cada archivo
